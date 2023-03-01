bffb8a267da98fb279fb331a6c595864fc284655
Vishal <31177415+tech-deity@users.noreply.github.com>
2023-03-01 21:55:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/restrauntappui/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.restrauntappui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFFFF4B3A)
+val primary_second = Color(0xFFFA4A0C)
+val white = Color(0xffffff)
+val black = Color(0xFF070707)
---
+++ app/src/main/java/com/example/restrauntappui/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.example.restrauntappui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = primary,
+    primaryVariant = white,
+    secondary = primary_second
+)
+
+private val LightColorPalette = lightColors(
+    primary = primary,
+    primaryVariant = primary_second,
+    secondary = white
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun RestrauntAppUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
